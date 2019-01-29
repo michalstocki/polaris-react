b49d7b67e942d9cfd5fc53a81c37e54f422d2a49
amrocha <andre.rocha@shopify.com>
2019-01-29 17:06:26 -0500

    I fixed it

--- .shopify-build/setup-consumer-test.sh
+++ .shopify-build/setup-consumer-test.sh
@@ -4,9 +4,17 @@ set -e
 
 ls -l
 mkdir ../tmp/polaris-react
-mv -v !(node_modules|web|polaris-styleguide) ../tmp/polaris-react
-mv -v ../tmp/polaris-react polaris-react
+mv !(node_modules|web|polaris-styleguide) ../tmp/polaris-react
+ls -l
+if [ -d "$1" ]; then
+  mv $1 $1tmp
+fi
+ls -l
 git clone ssh://git@github.com/Shopify/$1 --depth 1
+ls -l
+if [ -d "$1tmp" ]; then
+  mv $1tmp/* $1
+fi
 ls -l
 ls -l $1
 yarn --cwd ./$1 add file:../polaris-react
