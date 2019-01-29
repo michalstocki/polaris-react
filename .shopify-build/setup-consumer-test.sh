dfef17263e6f80621fcdd3d7c9207fabc89e3236
amrocha <andre.rocha@shopify.com>
2019-01-29 17:48:23 -0500

    i dont know about this

--- .shopify-build/setup-consumer-test.sh
+++ .shopify-build/setup-consumer-test.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 shopt -s extglob
-#set -e
+# set -e
+
+if [ ! -d "cache-directory" ]; then
+  mkdir cache-directory
+fi
 
 ls -l
 mkdir ../tmp/polaris-react
 mv !(node_modules|web|polaris-styleguide) ../tmp/polaris-react
-mv ../tmp/polaris-react polaris-react
 ls -l
-if [ -d "$1" ]; then
-  ls -l $1
-  mv $1/node_modules ./
-  rm -rf $1
-  git clone --depth 1 ssh://git@github.com/Shopify/$1
-  mv node_modules $1/
-else
-  git clone --depth 1 ssh://git@github.com/Shopify/$1
+git clone ssh://git@github.com/Shopify/$1 --depth 1
+if [ -d "cache-directory/$1" ]; then
+  cp -r cache-directory/$1/node_modules $1/node_modules
 fi
 ls -l
 ls -l $1
 yarn --cwd ./$1 add file:../polaris-react
+if [ -d "cache-directory/$1" ]; then
+  rm -rf cache-directory/$1
+fi
+mkdir cache-directory/$1
+mv $1/node_modules cache-directory/$1/node_modules
