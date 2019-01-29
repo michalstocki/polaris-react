94091022102bdff2aa7a3050e924d83dccd22d10
amrocha <andre.rocha@shopify.com>
2019-01-29 17:19:14 -0500

    im a dumass

--- .shopify-build/setup-consumer-test.sh
+++ .shopify-build/setup-consumer-test.sh
@@ -7,7 +7,7 @@ mkdir ../tmp/polaris-react
 mv !(node_modules|web|polaris-styleguide) ../tmp/polaris-react
 ls -l
 if [ -d "$1" ]; then
-  git clone --depth 1 ssh://git@github.com/Shopify/$1 ../tmp/
+  git clone --depth 1 ssh://git@github.com/Shopify/$1 ../tmp/$1
   mv ../tmp/$1/* $1
 else
   git clone --depth 1 ssh://git@github.com/Shopify/$1
